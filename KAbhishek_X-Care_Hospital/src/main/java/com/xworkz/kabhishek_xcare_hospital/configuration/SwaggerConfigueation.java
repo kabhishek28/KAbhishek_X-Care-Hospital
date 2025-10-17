c452d46937e222342042cea67f6d44636e25a53b
kabhishek28 <kabhishek.eng@gmail.com>
2025-10-17 10:39:20 +0530

    X-Care Hospital

---
+++ KAbhishek_X-Care_Hospital/src/main/java/com/xworkz/kabhishek_xcare_hospital/configuration/SwaggerConfigueation.java
@@ -0,0 +1,21 @@
+package com.xworkz.kabhishek_xcare_hospital.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfigueation {
+
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.xworkz.kabhishek_xcare_hospital.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
